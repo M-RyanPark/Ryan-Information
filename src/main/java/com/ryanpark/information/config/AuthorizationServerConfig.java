1869ae03208071ae721276326341596430910fef
Ryan <sanghyun.ryan.park@gmail.com>
2019-12-17 01:39:41 +0900

    oauth authorization server config 추가

---
+++ src/main/java/com/ryanpark/information/config/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2019 Ryan Information Test
+ */
+
+package com.ryanpark.information.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * @author : Sanghyun Ryan Park (sanghyun.ryan.park@gmail.com)
+ * @since : 2019-12-17
+ * description : oauth2 Authorization server config
+ */
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	AuthenticationManager authenticationManager;
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore())
+				.authenticationManager(authenticationManager)
+				.userDetailsService(userDetailsService)
+				.accessTokenConverter(accessTokenConverter())
+				.tokenStore(tokenStore());
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("123");
+		return converter;
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		return defaultTokenServices;
+	}
+}
---
+++ src/main/java/com/ryanpark/information/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Ryan Information Test
+ */
+
+package com.ryanpark.information.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author : Sanghyun Ryan Park (sanghyun.ryan.park@gmail.com)
+ * @since : 2019-12-17
+ * description : Spring Security Config
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				User.withDefaultPasswordEncoder()
+						.username("enduser")
+						.password("password")
+						.roles("USER")
+						.build()
+		);
+	}
+
+	@Bean("authenticationManagerBean")
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+}
