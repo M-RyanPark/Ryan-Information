eaafee7fba16373beb35ec303e5f9556a16df873
Ryan <sanghyun.ryan.park@gmail.com>
2019-12-23 02:37:22 +0900

    admin 계정 생성 스크립트 권한 정보 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,17 +4,30 @@
 
 INSERT INTO tb_account
 	(
-		user_id
+		user_no
+		, user_id
 		, password
 		, active
 		, status
 	)
 VALUES
 	(
-		'admin'
+		1
+		, 'admin'
 		, '$2a$10$hFQj6wJFFRECCqS4cjkJ7OzbKeYfWVjlS9ln12eXyZz3M0aP.BYTa'
 		, 'true'
 		, 'ACTIVE'
+	);
+
+INSERT INTO ACCOUNT_AUTHORITIES
+	(
+		account_user_no
+		, authority
+	)
+VALUES
+	(
+		1
+		, 'ADMIN'
 	);
 
 INSERT INTO oauth_client_details
@@ -35,7 +48,7 @@ VALUES
 	(
 		'loan_client',
 		'$2a$10$JZfpn0eI2UfXKvgdtUSENumr2Jr1xPmcErNzbtjpkf7blKzVGgmH.',
-		'spring-boot-application',
+		'information-api',
 		'read,write',
 		'authorization_code,rest_api,refresh_token,client_credentials',
 		'http://localhost:8080/callback',
