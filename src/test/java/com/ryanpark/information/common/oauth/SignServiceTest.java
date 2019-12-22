034768d894d9ab39ba729009e9a68c9494a87f42
Ryan <sanghyun.ryan.park@gmail.com>
2019-12-22 10:02:54 +0900

    SignServiceTest Slice 테스트로 변경

--- src/test/java/com/ryanpark/information/common/oauth/SignServiceTest.java
+++ src/test/java/com/ryanpark/information/common/oauth/SignServiceTest.java
@@ -4,7 +4,6 @@
 
 package com.ryanpark.information.common.oauth;
 
-import com.ryanpark.information.business.BusinessCommonTest;
 import com.ryanpark.information.common.domain.api.SignInRequest;
 import com.ryanpark.information.common.domain.api.SignInResponse;
 import com.ryanpark.information.common.domain.api.SignUpRequest;
@@ -12,30 +11,51 @@ import com.ryanpark.information.common.domain.api.SignUpResponse;
 import com.ryanpark.information.common.repository.entity.Account;
 import com.ryanpark.information.common.service.AccountService;
 import com.ryanpark.information.common.service.SignService;
+import com.ryanpark.information.common.service.impl.SignServiceImpl;
 import com.ryanpark.information.framework.exception.BadRequestException;
 import com.ryanpark.information.framework.exception.BusinessValidationException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.TokenRequest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author : Sanghyun Ryan Park (sanghyun.ryan.park@gmail.com)
  * @since : 2019-12-21
- * description : SignService Test 및 AuthorizationConfig Test
+ * description : SignService Test
  */
+@SpringBootTest(classes = SignServiceImpl.class)
 @Slf4j
-public class SignServiceAndAuthorizationConfigTest extends BusinessCommonTest {
+public class SignServiceTest {
 
 	@Autowired SignService signService;
+
 	@MockBean AccountService accountService;
+	@MockBean TokenGranter tokenGranter;
+
+	OAuth2AccessToken oAuth2AccessToken;
+
+	@BeforeEach
+	public void setup() {
+		oAuth2AccessToken = Mockito.mock(OAuth2AccessToken.class);
+
+		given(tokenGranter.grant(anyString(), any(TokenRequest.class))).willReturn(oAuth2AccessToken);
+	}
 
 	@Test
 	public void sign_up_success() {
@@ -45,11 +65,11 @@ public class SignServiceAndAuthorizationConfigTest extends BusinessCommonTest {
 				.willReturn(Account.of(signUpRequest.getUserId(), signUpRequest.getPassword()));
 
 		SignUpResponse response = signService.signUp(signUpRequest);
-		log.info("sign up response={}", response);
 
 		assertNotNull(response);
-		assertNotNull(response.getValue());
-		assertNotNull(response.getRefreshToken());
+
+		verify(accountService).createAccount(anyString(), anyString());
+		verify(tokenGranter).grant(anyString(), any(TokenRequest.class));
 	}
 
 	@Test
@@ -68,11 +88,10 @@ public class SignServiceAndAuthorizationConfigTest extends BusinessCommonTest {
 				.willReturn(Optional.of(Account.of(signInRequest.getUserId(), signInRequest.getPassword())));
 
 		SignInResponse response = signService.signIn(signInRequest);
-		log.info("sign in response={}", response);
 
 		assertNotNull(response);
-		assertNotNull(response.getValue());
-		assertNotNull(response.getRefreshToken());
+		verify(accountService).findAccount(anyString());
+		verify(tokenGranter).grant(anyString(), any(TokenRequest.class));
 	}
 
 	@Test
@@ -84,6 +103,8 @@ public class SignServiceAndAuthorizationConfigTest extends BusinessCommonTest {
 
 			signService.signIn(signInRequest);
 		});
+
+		verify(accountService).findAccount(anyString());
 	}
 
 	private SignUpRequest mockSignUpRequest(String userId) {
